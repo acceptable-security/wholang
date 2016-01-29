9a5f446bdd597246b457b435b2fed48ac018dffd
Brian Smith <block8437@gmail.com>
2016-01-29 10:56:24 -0500

    Removed some temporary build files. Need to figure out build situation

--- libwholex/src/lexer.o.d
+++
@@ -1,5 +0,0 @@
-../libwholex/src/lexer.o: \
-  /Users/avisaven/Documents/who/wholang/../libwholex/src/lexer.c \
-  /Users/avisaven/Documents/who/wholang/../libwholex/src/lexer.h
-/Users/avisaven/Documents/who/wholang/../libwholex/src/lexer.c:
-/Users/avisaven/Documents/who/wholang/../libwholex/src/lexer.h:
